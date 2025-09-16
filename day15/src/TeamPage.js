84aa52f4df44c480022aad79b731e215c4915b6e
Sneha2004879 <rajsneha551@gmail.com>
2025-09-16 20:15:36 +0530

    Adding day15

---
+++ day15/src/TeamPage.js
@@ -0,0 +1,9 @@
+import React, {useEffect,useState} from 'react';
+import api from './api';
+import { io } from 'socket.io-client';
+
+
+export default function TeamPage({ team, back }){
+const [tasks, setTasks] = useState([]);
+const socketRef = React.useRef();
+}
